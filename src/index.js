8eeb2590baccc371f10ae511095aad312814358e
Trapper1622 <titoutitiwow@gmail.com>
2018-08-03 10:26:13 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './containers/app'
+
+const API_KEY = "b3c502569d4b6f04beac56d2f9239b1e";
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
+
